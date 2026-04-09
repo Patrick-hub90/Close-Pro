09050b26c91ea595fab26d0cbe9f440f665732ed
Patrick-hub90 <77830730+Patrick-hub90@users.noreply.github.com>
2026-04-09 04:36:34 +0100

    Add service worker with basic event listeners

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(e) { self.skipWaiting(); });
+self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
+self.addEventListener('notificationclick', function(e) {
+  e.notification.close();
+  e.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(cs) {
+      if (cs.length > 0) { return cs[0].focus(); }
+      return clients.openWindow('/');
+    })
+  );
+});
